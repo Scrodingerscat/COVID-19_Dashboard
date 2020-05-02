0aa906114a730dedcda78625d6c54f1a486abdf8
Scrodingerscat <43383928+Scrodingerscat@users.noreply.github.com>
2020-05-02 16:55:24 -0400

    add clock and worker

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn covid19dashboard.app:server
+worker: python covid19dashboard.worker.py
+clock: python covid19dashboard.clock.py
--- covid19dashboard/worker.py
+++ covid19dashboard/worker.py
@@ -21,7 +21,6 @@ def dbupdate():
     conn.close()
     now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print("Database update has completed: ", now)
-    return now
 
 
 dbupdate()
